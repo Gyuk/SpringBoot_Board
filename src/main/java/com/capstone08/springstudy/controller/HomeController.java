8a887fb5221c759632d7509f0350c8369347160a
JungChanHyeong <chanh327@gmail.com>
2016-12-29 21:46:58 +0900

    16/12/29 21:46 commit

--- src/main/java/com/capstone08/springstudy/controller/HomeController.java
+++ src/main/java/com/capstone08/springstudy/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.capstone08.springstudy.controller;
 
+import com.capstone08.springstudy.data.PostRepository;
+import com.capstone08.springstudy.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private PostRepository postRepository;
 
     @RequestMapping("/")
-    public String Board() {
+    public String Board(ModelMap modelMap) {
+        List<Post> allPosts = postRepository.getALL_POSTS();
+        modelMap.put("posts", allPosts);
         return "home";
+    }
+
+    @RequestMapping("/write")
+    public String Write(){
+        return "write";
     }
 }
---
+++ src/main/java/com/capstone08/springstudy/model/Post.java
@@ -0,0 +1,72 @@
+package com.capstone08.springstudy.model;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Post {
+    private int no = 0;         // 번호
+    private String subject;     // 제목
+    private String nick;        // 글쓴이
+    private String content;     // 내용
+    private String date;          // 날짜
+    private int hit;            // 조회
+
+    public Post(int no, String subject, String nick, String content, int hit) {
+        this.no = no;
+        this.subject = subject;
+        this.nick = nick;
+        this.content = content;
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd");
+        this.date = dateFormat.format(new Date());
+        this.hit = hit;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public void setHit(int hit) {
+        this.hit = hit;
+    }
+}
